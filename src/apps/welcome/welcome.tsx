ae00f05a33b0e287844cac00e28018578e29b021
unknown <241-679-449@web.de>
2022-09-29 20:55:42 +0200

    img needs to be rouned ofc

--- src/apps/welcome/welcome.tsx
+++ src/apps/welcome/welcome.tsx
@@ -3,7 +3,7 @@ import logo from "./bff-hello.gif";
 export const Welcome = () => {
   return (
     <>
-      <img className="mx-auto h-20 w-20" src={logo} alt="loading..." />
+      <img className="mx-auto h-20 w-20 rounded-lg" src={logo} alt="loading..." />
     </>
   );
 };
